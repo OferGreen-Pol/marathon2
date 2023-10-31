75d25198dbc73692dcbdc093213320db5d87993d
unknown <OferGreen.Pol@gmail.com>
2023-10-31 12:41:59 +0200

    lesson 3

--- src/Bg.js
+++ src/Bg.js
@@ -1,6 +1,21 @@
 import './Bg.css';
+import { useState } from 'react';
 
 function Bg() {
+
+  const [display_no_bg_tab, setdisplay_no_bg_tab] = useState("no");
+
+//   const change_tab = () => {}
+
+  function change_tab() {
+    // debugger;
+    if(display_no_bg_tab==='yes') {
+        setdisplay_no_bg_tab('no');
+    } else {
+        setdisplay_no_bg_tab('yes');
+    }
+  }
+
   return (
     <div className="Bg">
         <div className="header">   
@@ -10,8 +25,20 @@ function Bg() {
         </div>
 
         <div className="main_div">   
-            <div className="left_div">   
-                LEFT
+
+            
+            <div className="left_div">
+                
+                <div className="main_div_tabs_header">   
+                    <span onClick={change_tab}> הסר רקע</span>
+                    <span onClick={change_tab}> מקורי</span>
+                </div>
+
+                {display_no_bg_tab==="yes" ? 
+                <div className="original_tab">original_tab</div>
+                :
+                <div className="no_bg_tab">no_bg_tab</div>
+                }
             </div>
                 
             <div className="right_div">   
@@ -22,6 +49,9 @@ function Bg() {
                         <button className="right_div_top_btn"> הורד</button>
                     </div>
                     <div className="right_div_bottom"></div>
+                        <div className="right_div_top_text"> תמונה חינם</div>
+                        <div className="right_div_top_subtext"> תמונה מלאה 1920x1080</div>
+                        <button className="right_div_top_btn"> הורד</button>
                 </div>
             </div>
         </div>
