66e1c705967a647c353d3351f17e480ca7f3901d
unknown <OferGreen.Pol@gmail.com>
2023-10-31 11:55:33 +0200

    lesson 2

--- src/Bg.js
+++ src/Bg.js
@@ -6,6 +6,24 @@ function Bg() {
         <div className="header">   
             <span className='header_text'> העלאת תמונה בשביל להסיר את הרקע  </span>
             <button className="header_btn"> העלאת תמונה   </button>
+            <span className="header_subtext">  פורמטים נתמכים png, jpeg</span>
+        </div>
+
+        <div className="main_div">   
+            <div className="left_div">   
+                LEFT
+            </div>
+                
+            <div className="right_div">   
+                <div className="right_div_middle_div">
+                    <div className="right_div_top">
+                        <div className="right_div_top_text"> תמונה חינם</div>
+                        <div className="right_div_top_subtext"> תצוגה מקדימה של תמונה 408x612</div>
+                        <button className="right_div_top_btn"> הורד</button>
+                    </div>
+                    <div className="right_div_bottom"></div>
+                </div>
+            </div>
         </div>
     </div>
   );
