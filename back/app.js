a4e0287b626d50886b69d09789beaf700f51844e
unknown <OferGreen.Pol@gmail.com>
2023-11-02 17:20:05 +0200

    lesson 7

--- back/app.js
+++ back/app.js
@@ -1,15 +1,18 @@
 const express = require('express')
+const path = require('path');
 const app = express()
 const port = 5000
+
+var fs = require('fs'); // filesystem (rename/move/copy functions)
 
 var cors = require('cors');1
 app.use(cors())
 
 
-var fileupload = require('express-fileupload');
+var fileupload = require('express-fileupload'); //file handling, gives req.files functionality
 app.use(fileupload());
 
-app.use(express.static('public'));
+app.use(express.static('upload_image')); // use folder_name where we want to access
 
 
 app.get('/test', (req, res) => {
@@ -19,8 +22,27 @@ app.get('/test', (req, res) => {
 
 app.post('/upload_file', (req, res) => {
       console.log(req.files)
+
+      const tempPath = req.files.path;
+      const targetPath = path.join(__dirname, "./upload_image/image.png");
+
+      console.log(tempPath);
+      console.log(targetPath);
+
+    //   fs.rename(tempPath, targetPath, err => {
+    //     if (err) return handleError(err,res);
+      
+
+    //   res
+    //     .status(200)
+    //     .contentType("text/plain")
+    //     .end("File uploaded!");
+
+    //     })
+
+    //     res.send("Hello World!")
     })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
--- src/Eula.js
+++ src/Eula.js
@@ -16,7 +16,7 @@ function Eula(props) {
     return (
         <div className='eula_div'>
             {/* onCLick of 'X' call local 'close_popup' which called parent 'close_popup to affect parent's code */}
-            <img src={close} onClick={close_popup}/>
+            <img src={close} onClick={close_popup} className='eula_close_img'/>
             לורם איפסום דולור סיט אמט, קונסקטטור אדיפיסינג אלית קולורס מונפרד אדנדום סילקוף, מרגשי ומרגשח. עמחליף לפרומי בלוף קינץ תתיח לרעח. לת צשחמי צש בליא, מנסוטו צמלח לביקו ננבי, צמוקו בלוקריה שיצמה ברורק. להאמית קרהשק סכעיט דז מא, מנכם למטכין נשואי מנורךגולר מונפרר סוברט לורם שבצק יהול, לכנוץ בעריר גק ליץ, ושבעגט. ושבעגט לבם סולגק. בראיט ולחת צורק מונחף, בגורמי מגמש. תרבנך וסתעד לכנו סתשם השמה - לתכי מורגם בורק? לתיג ישבעס.
         
         </div>
